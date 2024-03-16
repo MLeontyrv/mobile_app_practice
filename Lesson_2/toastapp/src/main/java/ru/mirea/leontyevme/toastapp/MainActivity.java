6c4da90717478888d9d991b8835b0988197d40dd
Maxim Leontyev <leontyev.maxim@mail.ru>
2024-03-16 19:46:22 +0300

    Lesson_2

--- Lesson_2/toastapp/src/main/java/ru/mirea/leontyevme/toastapp/MainActivity.java
+++ Lesson_2/toastapp/src/main/java/ru/mirea/leontyevme/toastapp/MainActivity.java
@@ -2,13 +2,27 @@ package ru.mirea.leontyevme.toastapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
     }
+
+    public void onClickToast(View view) {
+        int count = editText.getText().toString().length();
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "СТУДЕНТ №15 ГРУППА БСБО-04-22 Количество символов - " + count,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
 }
