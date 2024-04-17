178e6b70c8ab9e7840e39019fb23000cada2bfe7
Maxim Leontyev <leontyev.maxim@mail.ru>
2024-04-17 19:53:06 +0300

    Lesson_4

---
+++ MireaProject/app/src/main/java/ru/mirea/leontyevme/mireaproject/MyWorker.java
@@ -0,0 +1,20 @@
+package ru.mirea.leontyevme.mireaproject;
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+public class MyWorker extends Worker {
+    public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+
+        // TODO Фоновая задача
+
+        return Result.success();
+    }
+}
